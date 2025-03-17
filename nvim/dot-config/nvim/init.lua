a5d8c0bacca51295a0dcdd9dce69429a9b708d98
Pedro Prates <pap@uninova.pt>
2025-03-17 16:34:53 +0000

    added plugins folder to nvim. added treesitter and theme

--- nvim/dot-config/nvim/init.lua
+++ nvim/dot-config/nvim/init.lua
@@ -1,3 +1,11 @@
--- require("config.lazy")
-
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
 vim.wo.relativenumber = true
+
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.cursorline = true
+
+require("config.lazy")
+
+vim.cmd 'colorscheme kanagawa'
