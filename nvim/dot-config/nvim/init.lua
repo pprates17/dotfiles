7c0058bb1c55921df8f6e3970ad5921ccb565ad1
Pedro Prates <pap@uninova.pt>
2025-03-18 16:56:20 +0000

    Added smart-splits. Added d2 plugin to nvim

--- nvim/dot-config/nvim/init.lua
+++ nvim/dot-config/nvim/init.lua
@@ -21,4 +21,19 @@ vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live gr
 vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
 
-
+-- for example `10<A-h>` will `resize_left` by `(10 * config.default_amount)`
+vim.keymap.set('n', '<A-h>', require('smart-splits').resize_left)
+vim.keymap.set('n', '<A-j>', require('smart-splits').resize_down)
+vim.keymap.set('n', '<A-k>', require('smart-splits').resize_up)
+vim.keymap.set('n', '<A-l>', require('smart-splits').resize_right)
+-- moving between splits
+vim.keymap.set('n', '<C-h>', require('smart-splits').move_cursor_left)
+vim.keymap.set('n', '<C-j>', require('smart-splits').move_cursor_down)
+vim.keymap.set('n', '<C-k>', require('smart-splits').move_cursor_up)
+vim.keymap.set('n', '<C-l>', require('smart-splits').move_cursor_right)
+vim.keymap.set('n', '<C-\\>', require('smart-splits').move_cursor_previous)
+-- swapping buffers between windows
+vim.keymap.set('n', '<leader><leader>h', require('smart-splits').swap_buf_left)
+vim.keymap.set('n', '<leader><leader>j', require('smart-splits').swap_buf_down)
+vim.keymap.set('n', '<leader><leader>k', require('smart-splits').swap_buf_up)
+vim.keymap.set('n', '<leader><leader>l', require('smart-splits').swap_buf_right)
--- wezterm/dot-config/wezterm/wezterm.lua
+++ wezterm/dot-config/wezterm/wezterm.lua
@@ -1,4 +1,6 @@
 local wezterm = require 'wezterm'
+local ss = require 'smart_splits'
+local act = wezterm.action
 local config = wezterm.config_builder()
 
 -- Appearance
@@ -10,9 +12,20 @@ config.window_decorations = "RESIZE"
 config.leader = { key = 'a', mods = 'CTRL', timeout_milliseconds = 1000 }
 
 config.keys = {
-	{ key = '=', mods = 'LEADER', action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' }},
-	{ key = '-', mods = 'LEADER', action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' }},
-	{ key = 'w', mods = 'CTRL', action = wezterm.action.CloseCurrentPane { confirm = true }}
+	{ key = '=', mods = 'LEADER', action = act.SplitHorizontal { domain = 'CurrentPaneDomain' }},
+	{ key = '-', mods = 'LEADER', action = act.SplitVertical { domain = 'CurrentPaneDomain' }},
+	{ key = 'w', mods = 'LEADER', action = act.CloseCurrentPane { confirm = true }},
+	{ key = 't', mods = 'LEADER', action = act.SpawnTab('CurrentPaneDomain')},
+
+	-- Smart split functions
+	ss.split_nav('move', 'h'),
+	ss.split_nav('move', 'j'),
+	ss.split_nav('move', 'k'),
+	ss.split_nav('move', 'l'),
+	ss.split_nav('resize', 'h'),
+    	ss.split_nav('resize', 'j'),
+    	ss.split_nav('resize', 'k'),
+    	ss.split_nav('resize', 'l'),
 }
 
 return config
