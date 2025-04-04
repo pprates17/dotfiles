2d44eca1393396bdf35a41b0eb83a7806d36dfcf
Pedro Prates <pap@uninova.pt>
2025-04-04 17:48:25 +0100

    Added wezterm keybinds for tab naming. Added lua line

--- wezterm/dot-config/wezterm/wezterm.lua
+++ wezterm/dot-config/wezterm/wezterm.lua
@@ -9,7 +9,7 @@ config.color_scheme = 'Kanagawa (Gogh)'
 config.hide_tab_bar_if_only_one_tab = true
 config.window_decorations = "NONE"
 config.use_fancy_tab_bar = false
-
+config.tab_bar_at_bottom = true
 -- Keybinds
 config.leader = { key = 'a', mods = 'CTRL', timeout_milliseconds = 1000 }
 
@@ -17,7 +17,10 @@ config.keys = {
     { key = '=', mods = 'LEADER', action = act.SplitHorizontal { domain = 'CurrentPaneDomain' }},
     { key = '-', mods = 'LEADER', action = act.SplitVertical { domain = 'CurrentPaneDomain' }},
     { key = 'w', mods = 'LEADER', action = act.CloseCurrentPane { confirm = true }},
+    -- Tabs
     { key = 't', mods = 'LEADER', action = act.SpawnTab('CurrentPaneDomain')},
+    { key = 'n', mods = 'LEADER', action = act.ActivateTabRelative(1)},
+    { key = 'p', mods = 'LEADER', action = act.ActivateTabRelative(-1)},
     { key = '1', mods = 'CTRL', action = act.ActivateTab(0)},
     { key = '2', mods = 'CTRL', action = act.ActivateTab(1)},
     { key = '3', mods = 'CTRL', action = act.ActivateTab(2)},
@@ -27,7 +30,19 @@ config.keys = {
     { key = '7', mods = 'CTRL', action = act.ActivateTab(6)},
     { key = '8', mods = 'CTRL', action = act.ActivateTab(7)},
     { key = '9', mods = 'CTRL', action = act.ActivateTab(8)},
-    
+    { key = 'v', mods = 'LEADER', action = act.ActivateCopyMode},
+    { key = 'z', mods = 'LEADER', action = act.TogglePaneZoomState},
+    { key = ',', mods = 'LEADER', action = act.PromptInputLine {
+          description = 'Enter new name for tab',
+          action = wezterm.action_callback(
+            function(window, pane, line)
+              if line then
+                window:active_tab():set_title(line)
+              end
+            end
+          ),
+        },
+    },
     -- Smart split functions
     ss.split_nav('move', 'h'),
     ss.split_nav('move', 'j'), 
@@ -37,6 +52,8 @@ config.keys = {
     ss.split_nav('resize', 'j'),
     ss.split_nav('resize', 'k'),
     ss.split_nav('resize', 'l'),
+    
+
 }
 
 return config
