3477d3548fbc3b52a45df0372f8157f44a5aa59c
Pedro Prates <pap@uninova.pt>
2025-04-08 16:32:21 +0100

    Added resurect plugin

--- wezterm/dot-config/wezterm/wezterm.lua
+++ wezterm/dot-config/wezterm/wezterm.lua
@@ -1,7 +1,8 @@
 local wezterm = require 'wezterm'
 local ss = require 'smart_splits'
 local session_manager = require 'session-manager'
-
+local workspace_switcher = wezterm.plugin.require("https://github.com/MLFlexer/smart_workspace_switcher.wezterm")
+local resurrect = wezterm.plugin.require("https://github.com/MLFlexer/resurrect.wezterm")
 local act = wezterm.action
 local config = wezterm.config_builder()
 
@@ -13,9 +14,11 @@ config.window_decorations = "NONE"
 config.use_fancy_tab_bar = false
 config.tab_bar_at_bottom = true
     
-wezterm.on("save_session", function(window) session_manager.save_state(window) end)
-wezterm.on("load_session", function(window) session_manager.load_state(window) end)
-wezterm.on("restore_session", function(window) session_manager.restore_state(window) end)
+workspace_switcher.apply_to_config(config)
+wezterm.on('update-right-status', function(window, pane)
+    window:set_right_status(window:active_workspace())
+end)
+
 -- Keybinds
 config.leader = { key = 'a', mods = 'CTRL', timeout_milliseconds = 1000 }
 
@@ -41,7 +44,7 @@ config.keys = {
     { key = 'Space', mods = 'LEADER', action = act.RotatePanes 'Clockwise'},
     { key = '0', mods = 'LEADER', action = act.PaneSelect {mode = 'SwapWithActive'}},
     { key = ',', mods = 'LEADER', action = act.PromptInputLine {
-          description = 'Enter new name for tab',
+          description = 'Tab name',
           action = wezterm.action_callback(
             function(window, pane, line)
               if line then
@@ -51,13 +54,21 @@ config.keys = {
           ),
         },
     },
-    { key = 's', mods = 'LEADER', action = act.SwitchToWorkspace { name = 'default' }},
     { key = 'i', mods = 'LEADER', action = act.SwitchToWorkspace },
-    { key = '9', mods = 'ALT',    action = act.ShowLauncherArgs { flags = 'FUZZY|WORKSPACES'}},
-    { key = "y", mods = "LEADER", action = wezterm.action{EmitEvent = "save_session"}},
-    { key = "o", mods = "LEADER", action = wezterm.action{EmitEvent = "load_session"}},
-    { key = "p", mods = "LEADER", action = wezterm.action{EmitEvent = "restore_session"}},
-
+    { key = '.', mods = 'LEADER', action = act.PromptInputLine {
+          description = 'Workspace name',
+          action = wezterm.action_callback(
+            function(window, pane, line)
+              if line then
+		wezterm.mux.rename_workspace(
+		    wezterm.mux.get_active_workspace(),
+			line	
+		)
+              end
+            end
+          ),
+        },
+    },
     --Smart split functions
     ss.split_nav('move', 'h'),
     ss.split_nav('move', 'j'), 
@@ -67,11 +78,47 @@ config.keys = {
     ss.split_nav('resize', 'j'),
     ss.split_nav('resize', 'k'),
     ss.split_nav('resize', 'l'),
+    { key = "s", mods = "LEADER", action = workspace_switcher.switch_workspace()},
+    { key = "S", mods = "LEADER", action = workspace_switcher.switch_to_prev_workspace()},
+    { key = "w", mods = "ALT", action = wezterm.action_callback(function(win, pane)
+        resurrect.state_manager.save_state(resurrect.workspace_state.get_workspace_state())
+      end),
+    },
+    { key = "W", mods = "ALT", action = resurrect.window_state.save_window_action()},
+    { key = "T", mods = "ALT", action = resurrect.tab_state.save_tab_action()},
+    { key = "s", mods = "ALT", action = wezterm.action_callback(function(win, pane)
+	resurrect.state_manager.save_state(resurrect.workspace_state.get_workspace_state())
+        resurrect.window_state.save_window_action()
+	end)
+    },
+    {
+    key = "r",
+    mods = "ALT",
+    action = wezterm.action_callback(function(win, pane)
+      resurrect.fuzzy_loader.fuzzy_load(win, pane, function(id, label)
+        local type = string.match(id, "^([^/]+)") -- match before '/'
+        id = string.match(id, "([^/]+)$") -- match after '/'
+        id = string.match(id, "(.+)%..+$") -- remove file extention
+        local opts = {
+	    close_open_tabs = true,
+	    close_open_panes = true,
+	    window = pane:window(),
+  	    on_pane_restore = resurrect.tab_state.default_on_pane_restore,
+  	    relative = true,
+  	    restore_text = true,
+        }
+        if type == "workspace" then
+	    local state = resurrect.state_manager.load_state(id, "workspace")
+	    resurrect.workspace_state.restore_workspace(state, opts)
+        elseif type == "window" then
+	    local state = resurrect.state_manager.load_state(id, "window")
+	    resurrect.window_state.restore_window(pane:window(), state, opts)
+        elseif type == "tab" then
+	    local state = resurrect.state_manager.load_state(id, "tab")
+	    resurrect.tab_state.restore_tab(pane:tab(), state, opts)
+        end
+      end)
+    end),
+  },
 }
-    --local workspace_switcher = wezterm.plugin.require("https://github.com/MLFlexer/smart_workspace_switcher.wezterm")
-    --workspace_switcher.apply_to_config(config)
-    wezterm.on('update-right-status', function(window, pane)
-	window:set_right_status(window:active_workspace())
-    end)
-
 return config
