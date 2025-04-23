92975da6ebf746a688619898191b703ac4001d4b
Pedro Prates <pprates17@gmail.com>
2025-04-23 21:44:28 +0100

    Created basic neovim modular file structure. Added theme, treesitter and telescope plugins

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,5 +1,7 @@
 vim.g.mapleader = ' '
 vim.g.mapleader = ' '
+require("config.lazy")
+vim.cmd("colorscheme kanagawa-wave")
 vim.wo.relativenumber = true
 vim.wo.number = true
 vim.opt.shiftwidth = 4
