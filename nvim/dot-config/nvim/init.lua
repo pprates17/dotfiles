13b22456defd5df33975ca693243f9e32b71aaed
Pedro Prates <pprates17@gmail.com>
2025-04-24 14:51:07 +0100

    Renamed .config folder logic to dot-config, to be used with dotfiles option from stow

--- nvim/dot-config/nvim/init.lua
+++ nvim/dot-config/nvim/init.lua
@@ -5,3 +5,7 @@ vim.cmd("colorscheme kanagawa-wave")
 vim.wo.relativenumber = true
 vim.wo.number = true
 vim.opt.shiftwidth = 4
+vim.opt.colorcolumn = '80'
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.cursorline = true
