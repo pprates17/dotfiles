c5e95b651d5d2bd68366e26e1ddd3743100df146
Pedro Prates <pprates17@gmail.com>
2025-12-18 17:00:49 +0000

    Remigration to tmux

--- nvim/dot-config/nvim/lua/plugins/oil.lua
+++ nvim/dot-config/nvim/lua/plugins/oil.lua
@@ -5,7 +5,7 @@ return {
     lazy = false,
     config = function ()
 	local config = require("oil")
-	config:setup() 
+	config:setup()
 	vim.keymap.set('n','<leader>oo','<cmd>Oil<cr>', { desc = "Open parent directory" })
 	vim.keymap.set('n','<leader>op','<cmd>Oil --float<cr>', { desc = "Open parent directory float" })
     end,
