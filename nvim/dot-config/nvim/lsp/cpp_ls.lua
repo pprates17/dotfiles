e0f2e3fecbfaa1a1f1d9dd881e364fe5b014fb14
Pedro Prates <pprates17@gmail.com>
2026-02-20 18:00:41 +0000

    Added remote nvim experimental. Added options to clangd lsp

--- nvim/dot-config/nvim/lsp/cpp_ls.lua
+++ nvim/dot-config/nvim/lsp/cpp_ls.lua
@@ -1,5 +1,10 @@
 return {
-    cmd = {"clangd"},
+    cmd = {
+	"clangd",
+    	"--compile-commands-dir=build",
+    	"--query-driver=/home/pedro-prates/.espressif/tools/**",
+    	"--background-index",
+    },
     filetypes = {"cpp"},
     root_markers = {".cpp",".git"},
     settings = {
