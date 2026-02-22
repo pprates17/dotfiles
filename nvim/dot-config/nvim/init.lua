aa7f9636456545e54c81e2dad08f942ad1cb489e
Pedro Prates <pprates17@gmail.com>
2026-02-22 22:32:55 +0000

    Changed close window to Q. Removed deprecated themes.

--- nvim/dot-config/nvim/init.lua
+++ nvim/dot-config/nvim/init.lua
@@ -3,7 +3,7 @@ vim.g.maplocalleader = ' '
 vim.opt.termguicolors=true
 require("config.lazy")
 require("config.lsp")
-vim.cmd[[colorscheme tokyonight]]
+vim.cmd[[colorscheme tokyonight-night]]
 vim.wo.relativenumber = true
 vim.wo.number = true
 vim.opt.shiftwidth = 4
@@ -17,14 +17,14 @@ vim.wo.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
 vim.opt.foldenable = false
 
 -- Allow transparency
-vim.cmd [[
-  highlight Normal guibg=none
-  highlight NonText guibg=none
-  highlight NormalNC guibg=none
-  highlight Normal ctermbg=none
-  highlight NonText ctermbg=none
-  highlight NormalNC ctermbg=none
-]]
+--vim.cmd [[
+--  highlight Normal guibg=none
+--  highlight NonText guibg=none
+--  highlight NormalNC guibg=none
+--  highlight Normal ctermbg=none
+--  highlight NonText ctermbg=none
+--  highlight NormalNC ctermbg=none
+--]]
 
 local Terminal  = require('toggleterm.terminal').Terminal
 local lazygit = Terminal:new({ cmd = "lazygit", hidden = true, direction = "float" })
