736f7bd5421951362f9a52e30b4a0e76c9da49b2
Pedro Prates <pprates17@gmail.com>
2026-03-12 17:03:58 +0000

    Small fix in cpp_ls

--- nvim/dot-config/nvim/lsp/cpp_ls.lua
+++ nvim/dot-config/nvim/lsp/cpp_ls.lua
@@ -2,18 +2,10 @@ return {
     cmd = {
 	"clangd",
     	"--compile-commands-dir=build",
-    	"--query-driver=/home/pedro-prates/.espressif/tools/**",
+	"--query-driver=/home/pedro-prates/.espressif/tools/xtensa-esp-elf/esp-14.2.0_20251107/xtensa-esp-elf/bin/xtensa-esp-elf-gcc",
     	"--background-index",
+	"--clang-tidy"
     },
-    filetypes = {"cpp"},
-    root_markers = {".cpp",".git"},
-    settings = {
-	Lua = {
-	    diagnostics = {
-		globals = {
-		    "vim"
-		}
-	    }
-	}
-    }
+    filetypes = {"c", "cpp"},
+    root_markers = {"CMakeLists.txt",".git"}
 }
