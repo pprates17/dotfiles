a4e9df34b185e2026dfd7cfe2d538cd016c7c084
Pedro Prates <dev@pprates.com>
2026-04-29 16:42:29 +0100

    Added python and go to treesitter config

--- nvim/dot-config/nvim/lua/plugins/treesitter.lua
+++ nvim/dot-config/nvim/lua/plugins/treesitter.lua
@@ -4,7 +4,7 @@ return {
     config = function ()
 	local configs = require("nvim-treesitter.configs")
 	configs.setup({
-	    ensure_installed = { "c", "lua","css","jsonc","markdown" },
+	    ensure_installed = { "c", "lua","go","python","css","jsonc","markdown" },
             highlight = { enable = true },
 	    indent = { enable = true },
 	    incremental_selection = {
